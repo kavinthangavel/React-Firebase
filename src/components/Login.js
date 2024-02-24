b4cd3b3fb6bd9c47e6947c01eb18377659dda627
Kavin T <tdskavinkalkin@gmail.com>
2024-02-24 15:52:46 +0530

    Added Icons

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
 const Login = (props) => {
   const [email, setEmail] = useState('');
